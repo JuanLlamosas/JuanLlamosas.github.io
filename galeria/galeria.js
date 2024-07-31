5357f85fa761dce49c0db25f12a6e2f350595017
JuanLlamosas <llamosasjuan02@gmail.com>
2024-07-31 19:47:14 -0300

    CREACION DEL SITIO 31/7/24 - 19:47

---
+++ galeria/galeria.js
@@ -0,0 +1,19 @@
+//jQuery
+
+$('.galeria_img').click(function(e){
+
+    var img=e.target.src;
+    var modal= '<div class="modal" id="modal">   <img src="'+img+'" class="modal_img">    <div class="modal_boton" id="modalboton">X</div> </div>';
+   
+    $('body').append(modal);
+    $('#modalboton').click(function(){
+        $('#modal').remove();
+    });
+    
+});
+
+$(document).keyup(function(e){
+    if(e.which==27){
+        $('#modal').remove();
+    }
+});
